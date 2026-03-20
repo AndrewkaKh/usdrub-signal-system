faf5a1323899e1a016eb03885d7a92071a26db07
Andrew <aikhramov@edu.hse.ru>
2026-03-21 00:11:00 +0300

    added clean IV series export

--- moex_api.py
+++ moex_api.py
@@ -1,10 +1,12 @@
 from pprint import pprint
 
-from processing.iv import calculate_iv_snapshot, save_iv_snapshot
+from processing.iv import calculate_iv_snapshot, save_iv_showcase, save_iv_snapshot
 
 
 if __name__ == '__main__':
     snapshot = calculate_iv_snapshot(asset_code='Si')
-    save_path = save_iv_snapshot(snapshot)
+    snapshots_path = save_iv_snapshot(snapshot)
+    showcase_path = save_iv_showcase(snapshot)
     pprint(snapshot)
-    print(f'\nSnapshot saved to: {save_path}')
+    print(f'\nSnapshot saved to: {snapshots_path}')
+    print(f'IV showcase saved to: {showcase_path}')
--- processing/iv/__init__.py
+++ processing/iv/__init__.py
@@ -1,8 +1,11 @@
 from .service import calculate_iv_snapshot
+from .showcase import flatten_iv_showcase, save_iv_showcase
 from .storage import flatten_iv_snapshot, save_iv_snapshot
 
 __all__ = [
     'calculate_iv_snapshot',
     'flatten_iv_snapshot',
     'save_iv_snapshot',
+    'flatten_iv_showcase',
+    'save_iv_showcase',
 ]
