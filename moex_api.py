4f603b4715dd3d8201284488713ca24ac0be11ad
Andrew <aikhramov@edu.hse.ru>
2026-03-26 22:51:00 +0300

    drop iv_timeseries and hv_timeseries writes

--- moex_api.py
+++ moex_api.py
@@ -1,8 +1,8 @@
 from pprint import pprint
 
 from processing.features import save_feature_row
-from processing.hv import calculate_hv_snapshot, save_hv_showcase, save_hv_snapshot
-from processing.iv import calculate_iv_snapshot, save_iv_showcase, save_iv_snapshot
+from processing.hv import calculate_hv_snapshot, save_hv_snapshot
+from processing.iv import calculate_iv_snapshot, save_iv_snapshot
 
 
 if __name__ == '__main__':
@@ -10,11 +10,7 @@ if __name__ == '__main__':
     hv_snapshot = calculate_hv_snapshot(iv_snapshot)
 
     iv_snapshots_path = save_iv_snapshot(iv_snapshot)
-    iv_showcase_path = save_iv_showcase(iv_snapshot)
-
     hv_snapshots_path = save_hv_snapshot(hv_snapshot)
-    hv_showcase_path = save_hv_showcase(hv_snapshot)
-
     features_path = save_feature_row(iv_snapshot, hv_snapshot)
 
     print('IV snapshot:')
@@ -23,7 +19,5 @@ if __name__ == '__main__':
     pprint(hv_snapshot)
 
     print(f'\nIV snapshot saved to: {iv_snapshots_path}')
-    print(f'IV showcase saved to: {iv_showcase_path}')
     print(f'HV snapshot saved to: {hv_snapshots_path}')
-    print(f'HV showcase saved to: {hv_showcase_path}')
-    print(f'Features showcase saved to: {features_path}')
+    print(f'Features saved to: {features_path}')
--- processing/hv/__init__.py
+++ processing/hv/__init__.py
@@ -1,11 +1,8 @@
 from .service import calculate_hv_snapshot
-from .showcase import flatten_hv_showcase, save_hv_showcase
 from .storage import flatten_hv_snapshot, save_hv_snapshot
 
 __all__ = [
     'calculate_hv_snapshot',
     'flatten_hv_snapshot',
     'save_hv_snapshot',
-    'flatten_hv_showcase',
-    'save_hv_showcase',
 ]
